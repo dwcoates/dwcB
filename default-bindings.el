befb56d47cccc4514fcfa15e1e212d8d3dd39142
Dodge Coates <dcoates91@gmail.com>
2016-12-03 18:00:15 -0500

    Get rid of global map file

--- default-bindings.el
+++ default-bindings.el
@@ -120,7 +120,34 @@
               ;; SAVE
               (,(concat "C-" dwcB-kill-or-save-key)  .  save-buffer)
               (,(upcase dwcB-kill-or-save-key)    .     write-file)
-              (,(concat "M-" dwcB-kill-or-save-key)  .  save-some-bueffer))
+              (,(concat "M-" dwcB-kill-or-save-key)  .  save-some-bueffer)
+              ;; Ctrl-X map
+                                        ; general editing
+              (indent-rigidly)
+              (delete-blank-lines)
+              (mark-page)
+              (transpose-lines)
+              (repeat-complex-command)
+
+                                        ; register
+              (43 . increment-register)
+              (110 . number-to-register)
+              (103 . insert-register)
+              (120 . copy-to-register)
+              (106 . jump-to-register)
+              (32 . point-to-register)
+
+                                        ; rectangle
+              (78 . rectangle-number-lines)  ;; useful
+              (116 . string-rectangle)
+              (111 . open-rectangle)  ;; useful
+              (121 . yank-rectangle)
+              (100 . delete-rectangle) ;; useful
+              (107 . kill-rectangle)
+              (99 . clear-rectangle)
+              (rectangle-mark-mode)
+
+              )
  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
