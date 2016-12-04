d252fa5ed9d9dea951fdfb2ea757f5027e3b434a
Dodge W. Coates <dcoates91@gmail.com>
2016-12-04 13:40:37 -0500

    Finish default bindings update

--- default-bindings.el
+++ default-bindings.el
@@ -4,7 +4,7 @@
 ;;;;;;;;;;;;;;;;;;;;;; DWCB  GLOBAL ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; to add
+;;;; to add ;;;;
 ;
 ;(C-c . hide-entry)
 ;(C-t . hide-body)
@@ -65,51 +65,47 @@
                 )
               `())
 
- :wnd-binds `(
-              (,dwcB-forward-key  .  next-buffer)
-              (,dwcB-backward-key .  previous-buffer)
-              ("U"         .         switch-to-buffer-other-window)
-              ("u"         .         switch-to-buffer)
-              ("C-u"        .        helm-buffers-list)
-              ("o"         .         other-frame)
-              ("O"         .         other-window)
-              ("/"         .         dired-jump)
-              ("?"         .         dired-jump-other-window)
-              ;; Search
-              (,dwcB-search-alpha-key        .        ido-find-file)
-              ("C-" dwcB-search-alpha-key . ido-find-file-other-window)
-              (dwcB-search-alpha-key   .    ido-find-file-other-frame)
-              ;; Search
-              (,dwcB-search-alpha-key .  helm-mini)
-              (,dwcB-search-alpha-key .  helm-buffers-list)
-              (,dwcB-search-beta-key  .  helm-find-files)
-   ;;;;;;;;;;;;;;;;;;;;;
-   ;;;;;; EDITING ;;;;;;
-   ;;;;;;;;;;;;;;;;;;;;;
-              ;; RESIZE
-              (,dwcB-bigger-key         .          enlarge-window-horizontally)
-              (,dwcB-smaller-key         .         shrink-window-horizontally)
-              ("C-" dwcB-bigger-key  .   enlarge-window)
-              ("M-" dwcB-smaller-key  .  shrink-window)
-              ("b"                .                balance-windows)
-              ("B"                .                shrink-window-if-larger-than-buffer)
-              ("v"                .                golden-ratio)
-              ("V"                .                golden-ratio-mode)
-              ("z"                .                suspend-frame)
-              (dwcB-forward-key     .    split-window-right)
-              (dwcB-backward-key    .    split-horizontally)
-              (dwcB-upward-key     .     split-window-vertically)
-              (dwcB-downward-key    .    split-window-below)
-              ;; KILL
-              (,dwcB-kill-element-key             .            kill-buffer-and-window)
-              (,(upcase dwcB-kill-element-key)        .        kill-some-buffers)
-              ("C-" dwcB-kill-element-key     .      kill-buffer)
-              ("3 " (upcase dwcB-kill-element-key) . delete-other-frames)
-              ("3 " dwcB-kill-element-key     .      delete-frame)
-              ;; SAVE
-              ("C-" dwcB-kill-or-save-key  .  save-buffer)
-              (dwcB-kill-or-save-key    .     write-file)
-              ("M-" dwcB-kill-or-save-key  .  save-some-bueffer))
+ :wnd-binds `((
+              no-modifier    ((,dwcB-forward-key  .  next-buffer)
+                              (,dwcB-backward-key .  previous-buffer)
+                              (,dwcB-search-alpha-key        .        ido-find-file)
+                              ("U"         .         switch-to-buffer-other-window)
+                              ("u"         .         switch-to-buffer)
+                              ("C-u"        .        helm-buffers-list)
+                              ("o"         .         other-frame)
+                              ("O"         .         other-window)
+                              ("/"         .         dired-jump)
+                              ("?"         .         dired-jump-other-window)
+                              ("b"                .                balance-windows)
+                              ("B"                .                shrink-window-if-larger-than-buffer)
+                              ("v"                .                golden-ratio)
+                              ("V"                .                golden-ratio-mode)
+                              ("z"                .                suspend-frame)
+                              (,dwcB-search-alpha-key .  helm-mini)
+                              (,dwcB-search-alpha-key .  helm-buffers-list)
+                              (,dwcB-search-beta-key  .  helm-find-files)
+                              (,dwcB-bigger-key         .          enlarge-window-horizontally)
+                              (,dwcB-smaller-key         .         shrink-window-horizontally)
+                              (dwcB-forward-key     .    split-window-right)
+                              (dwcB-backward-key    .    split-horizontally)
+                              (dwcB-upward-key     .     split-window-vertically)
+                              (dwcB-downward-key    .    split-window-below)
+                              (,dwcB-kill-or-save-key  .  save-buffer)
+                              (,dwcB-kill-element-key             .            kill-buffer-and-window)
+                              (,(upcase dwcB-kill-element-key)        .        kill-some-buffers)
+                              )
+              C-modifier     ((,dwcB-search-alpha-key . ido-find-file-other-window)
+                              (,dwcB-bigger-key  .   enlarge-window)
+                              (,dwcB-kill-element-key     .      kill-buffer)
+                              (,dwcB-kill-element-key (upcase dwcB-kill-element-key) . delete-other-frames)
+                              ((upcase dwcB-kill-element-key) dwcB-kill-element-key     .      delete-frame)
+                              (dwcB-kill-or-save-key    .     write-file)
+
+                              )
+              M-modifier     ((,dwcB-smaller-key  .  shrink-window)
+                              (,dwcB-kill-or-save-key  .  save-some-buffers)))
+              `()
+              )
  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -119,9 +115,9 @@
 (dwcB-configure
  :key 'prog-map
  :base prog-mode-map
- :env-binds: `(
-               (,dwcB-note-key . comment-dwim)
-               )
+ :env-binds: `((
+                no-modifier ((,dwcB-note-key . comment-dwim))
+               ))
  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -132,33 +128,28 @@
 (dwcB-configure
  :key 'lisp-map
  :parent 'prog-map
- :env-binds `(;; Beg/End
-              (,dwcB-beginning-key . sp-beginning-of-sexp)
-              (,dwcB-end-key    .    sp-end-of-sexp)
-              ;; Upward/Downward
-              (,dwcB-downward-key       .       sp-down-sexp)
-              (,dwcB-upward-key        .        sp-up-sexp)
-              (,(concat "C-" dwcB-upward-key) . sp-backward-up-sexp)
-              ;; Forward/Backward
-              (,dwcB-forward-key             .             sp-forward-sexp)
-              (,dwcB-backward-key             .            sp-backward-sexp)
-              (,(concat "C-" dwcB-forward-key)      .      sp-forward-symbol)
-              (,(concat "C-" dwcB-backward-key)     .      sp-backward-symbol)
-              (,(concat "C-" (upcase dwcB-forward-key))  . sp-next-sexp)
-              (,(concat "C-" (upcase dwcB-backward-key)) . sp-previous-sexp)
-              ;; Slurp/Barf
-              ("e" . sp-forward-slurp-sexp)
-              ("r" . sp-forward-barf-sexp)
-              ("w" . sp-backward-slurp-sexp)
-              ("q" . sp-backward-barf-sexp)
-              ;; Transpose
-              (,dwcB-transpose-key . sp-transpose-sexp)
-              ;; Kill and Save
-              (,dwcB-kill-element-key            .             sp-kill-sexp)
-              (,(concat "C-" dwcB-kill-element-key)     .      sp-kill-hybrid-sexp)
-              (,(concat "C-" (upcase dwcB-kill-element-key)) . sp-backward-kill-sexp)
-              (,(concat "M-" dwcB-kill-element-key)      .     sp-copy-sexp)
-              )
+ :env-binds `(
+              (no-modifier    ((,dwcB-beginning-key . sp-beginning-of-sexp)
+                               (,dwcB-end-key    .    sp-end-of-sexp)
+                               (,dwcB-downward-key       .       sp-down-sexp)
+                               (,dwcB-upward-key        .        sp-up-sexp)
+                               (,dwcB-forward-key             .             sp-forward-sexp)
+                               (,dwcB-backward-key             .            sp-backward-sexp)
+                               ("e" . sp-forward-slurp-sexp)
+                               ("r" . sp-forward-barf-sexp)
+                               ("w" . sp-backward-slurp-sexp)
+                               ("q" . sp-backward-barf-sexp)
+                               (,dwcB-transpose-key . sp-transpose-sexp)
+                               (,dwcB-kill-element-key            .             sp-kill-sexp))
+               C-modifier     ((,dwcB-upward-key . sp-backward-up-sexp)
+                               (,dwcB-forward-key      .      sp-forward-symbol)
+                               (,dwcB-backward-key     .      sp-backward-symbol)
+                               (upcase dwcB-forward-key  . sp-next-sexp)
+                               (upcase dwcB-backward-key . sp-previous-sexp)
+                               (dwcB-kill-element-key     .      sp-kill-hybrid-sexp)
+                               ((upcase dwcB-kill-element-key) . sp-backward-kill-sexp))
+               M-modifier     ((dwcB-kill-element-key      .     sp-copy-sexp))
+               ))
  )
 
 
@@ -170,7 +161,7 @@
  :key 'emacs-lisp-mode
  :parent 'lisp-map
  :env-binds `(
-              (,dwcB-interpret-key . ielm)
+              (no-modifier ((,dwcB-interpret-key . ielm)))
               )
  )
 
@@ -191,23 +182,19 @@
  :key 'python-mode
  :parent 'prog-map
  :env-binds `(
-              ;; Search/Query
-              (,dwcB-query-key     .     elpy-doc)
-              (,dwcB-description-key  .  elpy-goto-definition)
-              (,dwcB-search-alpha-key .  elpy-rgrep-symbol)
-              ;; Direction
-              (,dwcB-forward-key         .         elpy-nav-forward-indent)
-              (,(concat "C-" dwcB-forward-key)  .  python-nav-forward-sexp)
-              (,(concat "M-" dwcB-forward-key)  .  elpy-nav-forward-block)
-              ;; Beg/End
-              (,dwcB-beginning-key        .        python-nav-beginning-of-statement)
-              (,(concat "C-" dwcB-end-key)    .    python-nav-beginning-of-block)
-              (,dwcB-end-key           .           python-nav-end-of-statement)
-              (,(concat "C-" dwcB-end-key)    .    python-nav-end-of-block)
-              ;; Interpreter/Compiler
-              (,dwcB-interpret-key         .         elpy-shell-switch-to-shell)
-              (,(concat "C-" dwcB-interpret-key)  .  elpy-shell-send-region-or-buffer)
-              (,(concat "M-" dwcB-interpret-key)  .  pyvenv-restart-python)
+              (no-modifier ((,dwcB-query-key     .     elpy-doc)
+                            (,dwcB-description-key  .  elpy-goto-definition)
+                            (,dwcB-search-alpha-key .  elpy-rgrep-symbol)
+                            (,dwcB-forward-key         .         elpy-nav-forward-indent)
+                            (,dwcB-beginning-key        .        python-nav-beginning-of-statement)
+                            (,dwcB-end-key           .           python-nav-end-of-statement)
+                            (,dwcB-interpret-key         .         elpy-shell-switch-to-shell))
+               C-modifier  ((dwcB-forward-key  .  python-nav-forward-sexp)
+                            (dwcB-end-key    .    python-nav-beginning-of-block)
+                            (dwcB-end-key    .    python-nav-end-of-block)
+                            (dwcB-interpret-key  .  elpy-shell-send-region-or-buffer))
+               M-modifier  ((dwcB-forward-key  .  elpy-nav-forward-block)
+                            (dwcB-interpret-key  .  pyvenv-restart-python)))
               )
  )
 
@@ -218,25 +205,19 @@
 (dwcB-configure
  :key 'org-mode
  :gen-binds `(
-              ;; Kill
-              (,(concat "C-" dwcB-kill-big-key)  . org-kill-line)
-              ;; Beg/End
-              (,(concat "C-" dwcB-beginning-key) . org-beginning-of-line)
-              (,(concat "C-" dwcB-end-key)    .    org-end-of-line)
+              (C-modifier  ((dwcB-kill-big-key  . org-kill-line)
+                            (dwcB-beginning-key . org-beginning-of-line)
+                            (dwcB-end-key    .    org-end-of-line)))
               )
  :env-binds `(;; Direction
-              (,(concat "C-" dwcB-forward-key)   .  org-forward-heading-same-level)
-              (,(concat "C-" dwcB-backward-key)  .  org-previous-heading-same-level)
-              (,(concat "C-" dwcB-upward-key)   .   outline-next-visible-heading)
-              (,(concat "C-" dwcB-downward-key)  .  outling-previous-visible-heading)
-              ;; Beg/Eng
-              ;; Bigger/Smaller
-              (,dwcB-bigger-key         .          widen)
-              (,dwcB-smaller-key         .         org-narrow-to-element)
-              (,(concat "C-" dwcB-smaller-key)  .  org-narrow-to-subtree)
-              (,(concat "C-" dwcB-smaller-key)  .  org-narrow-to-block)
-              ;; Search
-              ;; Misc
+              (no-modifier ((,dwcB-bigger-key         .          widen)
+                            (,dwcB-smaller-key         .         org-narrow-to-element))
+               C-modifier  ((dwcB-forward-key   .  org-forward-heading-same-level)
+                            (dwcB-backward-key  .  org-previous-heading-same-level)
+                            (dwcB-upward-key   .   outline-next-visible-heading)
+                            (dwcB-downward-key  .  outling-previous-visible-heading)
+                            (dwcB-smaller-key  .  org-narrow-to-subtree)
+                            (dwcB-smaller-key  .  org-narrow-to-block)))
               )
  )
 
@@ -248,13 +229,12 @@
  :key 'helm--minor-mode
  :parent 'helm-map
  :gen-binds `(
-              ;; Direction
-              (,(concat "C-" dwcB-downward-key)  .  helm-next-line)
-              (,(concat "H-" dwcB-upward-key)   .   helm-previous-line)
-              (,(concat "C-" dwcB-forward-key)   .  helm-next-source)
-              (,(concat "C-" dwcB-backward-key)  .  helm-previous-source)
-              (,(concat "M-" dwcB-downward-key)  .  helm-next-page)
-              (,(concat "M-" dwcB-upward-key)   .   helm-previous-page)
+              (C-modifier ((dwcB-downward-key  .  helm-next-line)
+                           (dwcB-upward-key   .   helm-previous-line)
+                           (dwcB-forward-key   .  helm-next-source)
+                           (dwcB-backward-key  .  helm-previous-source))
+               M-modifier ((dwcB-downward-key  .  helm-next-page)
+                           (dwcB-upward-key   .   helm-previous-page)))
               )
  )
 
